681a39f068bdd85878771369ad10ed1c5b43c898
sathishmepco53@gmail.com <sathish@greenboards.in>
2018-04-17 15:04:35 +0530

    header is modified

--- src/main/java/com/sathish/webhook/controller/HerokuController.java
+++ src/main/java/com/sathish/webhook/controller/HerokuController.java
@@ -1,5 +1,6 @@
 package com.sathish.webhook.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -7,6 +8,7 @@ import javax.ws.rs.core.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,19 +17,23 @@ import com.sathish.webhook.intents.MainHandler;
 @RestController
 public class HerokuController {
 	
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	@RequestMapping(value = "/call", method = RequestMethod.POST,headers = "Accept=application/json", produces = "application/json",consumes="application/json")
-	public @ResponseBody String getInput(@RequestBody String request){
+//	@Consumes(MediaType.APPLICATION_JSON)
+//	@Produces(MediaType.APPLICATION_JSON)
+//	, produces = "application/json",consumes="application/json"
+//	,headers = "Content-Type=application/x-www-form-urlencoded"
+	@RequestMapping(value = "/call", method = RequestMethod.POST)
+	public @ResponseBody String getInput(@RequestParam String data){
 		System.out.println("Inside Call");
-		System.out.println("Request Received is :: "+request);
+//		System.out.println("Request Received is :: "+request.getParameter("data"));
+		System.out.println("Request Received is :: "+data);
 		//return "{	color: \"red\",	value: \"#f00\"}";
 		//return "{\"fulfillmentText\": \"questions\",\"fulfillmentMessages\": [{\"speech\": \"result from Heroku\", \"displayText\": \"result form heroku\"}],\"source\": string,\"payload\": {\"payload from heroku\"}}";
 		//String response = "{\"fulfillmentText\": \"questions\",\"fulfillmentMessages\": [{\"speech\": \"result from Heroku\", \"displayText\": \"result form heroku\"}]";
 		
 		/*String response = "{\"fulfillmentText\": \"result from Heroku\"}";
 		System.out.println("Sending Response: "+response);*/
-		MainHandler mainHandler = new MainHandler(request);
+//		MainHandler mainHandler = new MainHandler(request.getParameter("data"));
+		MainHandler mainHandler = new MainHandler(data);
 		String response= mainHandler.process();
 		System.out.println("Sending Response: "+response);
 		return response;
